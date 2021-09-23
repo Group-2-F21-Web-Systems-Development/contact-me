cefd7bd99e846aca46e70fdfb3ae87e001c90805
Cosillow <silloc@rpi.edu>
2021-09-23 15:44:48 -0400

    created and styled footer.php
    Using php, we only have to create the footer in one file, then we can include it in as many files we want across our site. Because the footer will be on almost all of our pages, this minimizes how much html we have to write in the future. (This was a very basic implementation, I expect the style and possibly the html to evolve throughout our project)

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php echo 
+
+/* 
+  Add the following to the bottom of any php file to include the footer:
+  <?php include 'footer.php'; ?> 
+*/
+
+
+"
+<footer>
+  <button>groups</button>
+  <button>connections</button>
+  <button>profile</button>
+</footer>
+"
+
+
+?>
+
