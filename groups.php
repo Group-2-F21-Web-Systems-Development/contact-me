fa4d378185668e078cbf2d7c2282ffd2ee505d8d
Cosillow <silloc@rpi.edu>
2021-10-12 15:42:50 -0400

    created fake data for groups page and added defer tags for javascript script tags
    we just learned that defer is better for front end optimization

--- groups.php
+++ groups.php
@@ -5,33 +5,73 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contact Me Groups Page</title>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="  crossorigin="anonymous"></script>
+  <script defer src="https://code.jquery.com/jquery-3.6.0.min.js"  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="  crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./src/styles/style.css">
   <link rel="stylesheet" href="./src/styles/groups.css">
-  <script src="./src/ajax/ajax.js"></script>
+  <script defer src="./src/ajax/ajax.js"></script>
 </head>
 <body>
   <section class="groups">
     <h1>View Your Groups</h1>
     <div class="group">
-      <img src="./src/img/activities_fair.jpg" alt="photo of group __">
+      <img src="./src/img/activities_fair.jpg" alt="photo of RPI Activities Fair">
       <h2><a href="#">RPI Activities Fair</a></h2>
       <p class="people">300 people</p>
     </div>
     <div class="group">
-      <img src="./src/img/activities_fair.jpg" alt="photo of group __">
+      <img src="./src/img/tree.jpg" alt="photo of Enviornmental Society Meet-up">
+      <h2><a href="#">Enviornmental Society Meet-up</a></h2>
+      <p class="people">95 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/golf.jpg" alt="photo of Geiss Country Club Networking Event">
+      <h2><a href="#">Geiss Country Club Networking Event</a></h2>
+      <p class="people">2,000 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/monkey.jpg" alt="photo of Monkey Appreciation Club Interest Meeting">
+      <h2><a href="#">Monkey Appreciation Club Interest Meeting</a></h2>
+      <p class="people">22 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/activities_fair.jpg" alt="photo of RPI Activities Fair">
       <h2><a href="#">RPI Activities Fair</a></h2>
       <p class="people">300 people</p>
     </div>
     <div class="group">
-      <img src="./src/img/activities_fair.jpg" alt="photo of group __">
+      <img src="./src/img/tree.jpg" alt="photo of Enviornmental Society Meet-up">
+      <h2><a href="#">Enviornmental Society Meet-up</a></h2>
+      <p class="people">95 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/golf.jpg" alt="photo of Geiss Country Club Networking Event">
+      <h2><a href="#">Geiss Country Club Networking Event</a></h2>
+      <p class="people">2,000 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/monkey.jpg" alt="photo of Monkey Appreciation Club Interest Meeting">
+      <h2><a href="#">Monkey Appreciation Club Interest Meeting</a></h2>
+      <p class="people">22 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/activities_fair.jpg" alt="photo of RPI Activities Fair">
       <h2><a href="#">RPI Activities Fair</a></h2>
       <p class="people">300 people</p>
     </div>
     <div class="group">
-      <img src="./src/img/activities_fair.jpg" alt="photo of group __">
-      <h2><a href="#">RPI Activities Fair</a></h2>
-      <p class="people">300 people</p>
+      <img src="./src/img/tree.jpg" alt="photo of Enviornmental Society Meet-up">
+      <h2><a href="#">Enviornmental Society Meet-up</a></h2>
+      <p class="people">95 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/golf.jpg" alt="photo of Geiss Country Club Networking Event">
+      <h2><a href="#">Geiss Country Club Networking Event</a></h2>
+      <p class="people">2,000 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/monkey.jpg" alt="photo of Monkey Appreciation Club Interest Meeting">
+      <h2><a href="#">Monkey Appreciation Club Interest Meeting</a></h2>
+      <p class="people">22 people</p>
     </div>
   </section>
 </body>
