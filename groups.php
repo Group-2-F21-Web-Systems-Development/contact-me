9dc2e18ea0cdb1bd06f1d38896b2b831b74a50f7
Cosillow <silloc@rpi.edu>
2021-10-12 12:09:47 -0400

    Populated fake data and initially styled group page
    every other group has a lowered opacity before of the page's accent color, which I think nicely separates the groups. Everything else uses display flex to nicely lay out the groups page.

--- groups.php
+++ groups.php
@@ -7,10 +7,32 @@
   <title>Contact Me Groups Page</title>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="  crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./src/styles/style.css">
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="  crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./src/styles/groups.css">
   <script src="./src/ajax/ajax.js"></script>
 </head>
 <body>
-  
+  <section class="groups">
+    <h1>View Your Groups</h1>
+    <div class="group">
+      <img src="./src/img/activities_fair.jpg" alt="photo of group __">
+      <h2><a href="#">RPI Activities Fair</a></h2>
+      <p class="people">300 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/activities_fair.jpg" alt="photo of group __">
+      <h2><a href="#">RPI Activities Fair</a></h2>
+      <p class="people">300 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/activities_fair.jpg" alt="photo of group __">
+      <h2><a href="#">RPI Activities Fair</a></h2>
+      <p class="people">300 people</p>
+    </div>
+    <div class="group">
+      <img src="./src/img/activities_fair.jpg" alt="photo of group __">
+      <h2><a href="#">RPI Activities Fair</a></h2>
+      <p class="people">300 people</p>
+    </div>
+  </section>
 </body>
 </html>
--- profile.php
+++ profile.php
@@ -7,7 +7,6 @@
   <title>Contact Me Profile Page</title>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="  crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./src/styles/style.css">
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="  crossorigin="anonymous"></script>
   <script src="./src/ajax/ajax.js"></script>
 </head>
 <body>
