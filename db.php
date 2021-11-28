a74517e510137d263cd79494d1f3f8ec49cc950e
alanabarth <bartha4@rpi.edu>
2021-11-27 22:50:56 -0500

    made login and sign up dynamic
    added backend for connecting to the database with login credentials and sign up working

---
+++ db.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html>
+<head>
+   <title>Lab 8</title>
+   <script type="text/javascript" src="jquery3.6.0.min.js"></script>
+   <link rel="stylesheet" href="style.css">
+</head>
+</html>
+<?php 
+session_start(); 
+
+$dbusername= "root";
+$dbpassword = "group2websys";
+
+$conn = new PDO('mysql:host=localhost;dbname=contactme',$dbusername, $dbpassword);
+if (!$conn) {
+    echo "Connection failed!";
+}
+
+if (isset($_POST['username']) && isset($_POST['pass'])) {
+    function validate($data) {
+       $data = trim($data);
+       $data = stripslashes($data);
+       $data = htmlspecialchars($data);
+       return $data;
+    }
+
+    $uname = validate($_POST['username']);
+    $pass = validate($_POST['pass']);
+
+    if (empty($uname)) {
+        header("Location: login.php?error=User Name is required");
+        exit();
+    } else if(empty($pass)) {
+        header("Location: login.php?error=Password is required");
+        exit();
+    } else {
+        $sql = "select * from users where username=:uname";
+        $result = $conn->prepare($sql);
+        $result->execute(array(':uname'=> $uname));
+        $count = $result->rowCount();
+
+        if ($count === 1) {
+            $results = $result->fetchAll();
+            foreach($results as $row){
+               if(password_verify($pass, $row['pass'])) {
+                  echo "Logged in!";
+                  $_SESSION['username'] = $row['username'];
+                  $_SESSION['fname'] = $row['fname'];
+                  $_SESSION['lname'] = $row['lname'];
+                  $_SESSION['fname'] = $row['fname'];
+                  $_SESSION['email'] = $row['email'];
+                  $_SESSION['links'] = $row['links'];
+                  $_SESSION['is_admin'] = $row['is_admin'];
+                  $_SESSION['photo_location'] = $row['photo_location'];
+                  $_SESSION['id'] = $row['id'];
+                  header("Location: personalprofile.php");
+                  exit();
+               } else {
+                  header("Location: login.php?error=Incorect credentials");
+                  exit();
+               }
+            }
+        } else {
+            #echo "did not return 1";
+            header("Location: login.php?error=Incorect credentials");
+            exit();
+    }
+  }
+} else {
+  header("Location: index.php");
+  exit();
+}
+?>
--- sign-up.php
+++ sign-up.php
@@ -13,7 +13,7 @@
 <body>
    <div id="bodyBlock">
       <h1>Sign up</h1>
-      <form id="addForm" name="addForm" action="#" method="post" onsubmit="return validate(this);">
+      <form id="addForm" name="addForm" action="sign-up.php" method="post">
         <fieldset> 
           <legend>Contact Information</legend>
           <div class="formData">
@@ -25,14 +25,84 @@
             <div class="value"><input name="last-name" id="last-name" type="text"></div>
             <label for="email">Email:</label>
             <div class="value"><input name="email" id="email" type="text"></div>
+            <label for="username">Username:</label>
+            <div class="value"><input name="username" id="username" type="text"></div>
             <label for="password">Password:</label>
             <div class="value"><input name="password" id="password" type="text"></div>
             <label for="confirm">Confirm Password:</label>
             <div class="value"><input name="confirm" id="confirm" type="text"></div>
           </div>
-          <button onclick="clickMe()">Sign Up</button>
+          <button type="submit">Sign Up</button>
         </fieldset>
       </form>
    </div>
 </body>
 </html>
+
+<?php 
+session_start(); 
+
+$dbusername= "root";
+$dbpassword = "group2websys";
+
+$conn = new PDO('mysql:host=localhost;dbname=contactme',$dbusername, $dbpassword);
+if (!$conn) {
+    echo "Connection failed!";
+}
+
+if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['first-name']) && isset($_POST['last-name']) && isset($_POST['confirm'])) {
+    function validate($data) {
+       $data = trim($data);
+       $data = stripslashes($data);
+       $data = htmlspecialchars($data);
+       return $data;
+    }
+
+    $uname = validate($_POST['username']);
+    $pass = validate($_POST['password']);
+    $email = validate($_POST['email']);
+    $fname = validate($_POST['first-name']);
+    $lname = validate($_POST['last-name']);
+    $confirm = validate($_POST['confirm']);
+    echo $uname . ' ' . $pass . ' ' . $email . ' ';
+
+
+    if (empty($uname)) {
+        header("Location: sign-up.php?error=Username is required");
+        exit();
+    } else if(empty($email)) {
+        header("Location: sign-up.php?error=email is required");
+        exit();
+    } else if(empty($pass)) {
+        header("Location: sign-up.php?error=Password is required");
+        exit();
+    } else if(empty($fname)) {
+        header("Location: sign-up.php?error=First Name is required");
+        exit();
+    } else if(empty($lname)) {
+        header("Location: sign-up.php?error=Last Name is required");
+        exit();
+    } else if($pass != $confirm){
+        header("Location: sign-up.php?error=Passwords don't match");
+        exit();
+    } else {
+      $sql = "select * from users where username=:uname";
+      $result = $conn->prepare($sql);
+      $result->execute(array(':uname'=> $uname));
+      $count = $result->rowCount();
+
+      if ($count === 1) {
+            header("Location: sign-up.php?error=username already exists");
+            exit();
+      }else{
+        $sql = "insert into users(username, pass, email, fname, lname) VALUES(:uname, :pass, :email, :fname, :lname)";
+        $result = $conn->prepare($sql);
+        $pass = password_hash($pass, PASSWORD_BCRYPT);
+        echo 'user created!';
+        $result->execute(array(':uname'=> $uname, ':pass' => $pass, ':email' => $email, ':fname' => $fname, ':lname' => $lname));
+        header("Location: login.php");
+        exit();
+      }
+    }
+}
+?>
