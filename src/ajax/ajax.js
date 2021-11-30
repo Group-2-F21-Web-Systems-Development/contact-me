ac2b57ad53d205ba44aeadc98c2e90fe3e880154
Cosillow <silloc@rpi.edu>
2021-11-30 11:43:21 -0500

    created a php and javascript (ajax) system for logging out
    JS makes a synchronous call to php function that gives session super global as a json object. Then, if user has a session it shows a logout option instead of login and signup.

--- src/ajax/ajax.js
+++ src/ajax/ajax.js
@@ -3,7 +3,17 @@ $(document).ready(function() { // after everything in html loads
     type: "GET",
     url: "./src/ajax/data.json",
     dataType: "json",
+    cache: "false",
     success: function(responseData, status) {
+      // acquire php session super global as json object
+      var sessionExists = 0;
+      $.ajaxSetup({cache: false, async: false})
+      $.get('./src/ajax/getsession.php', function (data) {
+        jsObj =  JSON.parse(data);
+        if (!(Object.keys(jsObj).length === 0)) {
+          sessionExists = 1;
+        }
+      });
       // header
       header = document.createElement("header");
       nav = document.createElement("nav");
@@ -12,8 +22,18 @@ $(document).ready(function() { // after everything in html loads
         li = document.createElement("li");
         // create anchor tags nested in li with text and href
         a = document.createElement("a");
-        a.append(Object.keys(liEl));
-        a.setAttribute("href", Object.values(liEl));
+        if ( i === 3 && sessionExists === 1) {
+          // login
+
+        } else if ( i === 4 && sessionExists === 1) {
+          // signup
+          a.append("Logout");
+          a.setAttribute("href", "./src/ajax/logout.php");
+        } else {
+          a.append(Object.keys(liEl));
+          a.setAttribute("href", Object.values(liEl));
+        }
+        
         if (i==0) { // add logo class
           a.classList.toggle("logo");
         }
