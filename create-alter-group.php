6ba2315e8464ed717479d57f1617a05dd16118f8
filawj <filawj@rpi.edu>
2021-11-30 11:48:59 -0500

    Update file
    Trying to work on if statements. Need to get if statements to work so that users can make changes and save them

--- create-alter-group.php
+++ create-alter-group.php
@@ -1,3 +1,64 @@
+  <!-- empty form group(users are creating new group) -->
+  <?php 
+  session_start();
+        // connect to databse
+        $dbusername= "root";
+        $dbpassword = "group2websys";
+        
+        $conn = new PDO('mysql:host=localhost;dbname=contactme',$dbusername, $dbpassword, array(PDO::MYSQL_ATTR_FOUND_ROWS => true));
+        if (!$conn) {
+            echo "Connection failed!";
+        }
+  
+        // <!-- only works if the url has error=  -->
+        // <!-- text input instead of p tag -->
+        // <!-- user is creating a new group -->
+        // true is coming from groups.php and it is getting the url from line 41
+  if (isset($_GET['group'])){
+    // user is accessing a group that already exists 
+    $title=$_GET['group'];
+    // look for anything to write sql commands that is looking for the group, specified from the title
+    // save the information into php variables and display onto the form/website
+    // ask the database for the group with that title 
+    // look at lab 7 (look at the README)
+  }
+  if (isset($_GET['new']) && $_GET['new']==='true')  { 
+    // write in the specific fields (monkeys)
+    // user is creating a new group, which is inserting a new row into the database 
+    // creating a new group
+    $title='';
+    $description='';
+
+
+
+    // connecting to database
+    $title=mysqli_real_escape_string($conn, $_GET['new']);
+    $description=mysqli_real_escape_string($conn, $_GET['new']);
+
+    // create sql
+    // insert into the groups table and we want to update these columns (title and description) and these are the values for those columns
+    $sql="INSERT INTO groups(title, description) VALUES('$title,'$description');"
+    // way to set up the query
+    $result = $conn->prepare($sql);
+    prepare = 
+    $result->execute();
+    // save to db and check to see if the everything is saved
+    // this will be a success and check is if everything is true
+    if {
+      // success
+    }
+    else{
+      echo "Changes not saved!".mysqli_error($conn);
+    }
+
+      // <!-- only works if the url has error=  -->
+      // <!-- text input instead of p tag -->
+ <input class="new"><?php echo $_GET['new']; ?></input>
+
+    <?php } ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +76,7 @@
     <h1><span>Edit Group:</span> Monkey Apreciation Club Interest Meeting</h1>
     <form>
       <label for="title">Title</label>
+      // include php inside of value
       <input id="title" name="title" type="text" value="Monkey Apreciation Club Interest Meeting">
       <label for="description">Description</label>
       <textarea id="description" name="description" cols="30" rows="10">Some primate species are recognized for their tree-swinging leaps that moved being acrobats to shame! Some monkey species take this “ arm at arm ” technique you may have seen children practicing on that “ monkey bars ” in the yard! Colobus monkeys, unlike other primate species, have hind legs that are much further than their forelimbs, creating for unbelievable leaping power with good velocity. In other words, monkeys are cool so come to our club interest meeting</textarea>
@@ -26,31 +88,13 @@
 </body>
 </html>
 
+// put php after the document 
 
-<?php if (isset($_GET['new'])) { ?>
-  $title=$_GET['new'];
-  $description=$_GET['new'];
-  header("Location: create-alter-group.php?new=Fill in title and description");
+
+
   
-  // echo "Changes saved!"
-};
-
-<?php } ?>
-      <!-- only works if the url has error=  -->
-      <!-- text input instead of p tag -->
-      <input class="new"><?php echo $_GET['new']; ?></p>
-
-    <?php if (isset($_GET['new'])) { ?>
-      $title=$_GET['new'];
-  $description=$_GET['new'];
-  header("Location: create-alter-group.php?new=Fill in title and description");
-
-      <!-- only works if the url has error=  -->
-      <!-- text input instead of p tag -->
-      <p class="error"><?php echo $_GET['error']; ?></p>
-    <?php } ?>
 
 
 
-<!-- make if statements -->
-<!-- make sure the ids are the same and only change the data that needs to be changed     -->
+// <!-- make if statements -->
+// <!-- make sure the ids are the same and only change the data that needs to be changed     -->
