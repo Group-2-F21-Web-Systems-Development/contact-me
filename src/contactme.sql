f25fe35e6cb2b7a1a0c4824354843e6a962707a8
filawj <filawj@rpi.edu>
2021-12-03 11:46:09 -0500

    Update file
    Replaced sql file with a new one.

--- src/contactme.sql
+++ src/contactme.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2021 at 05:03 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
+-- Generation Time: Dec 03, 2021 at 05:45 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `groups` (
   `title` varchar(255) NOT NULL,
   `description` longtext DEFAULT NULL,
   `created_by` int(11) NOT NULL,
-  `photo_location` longtext DEFAULT 'uploads/48724i2bsakfnsaoidjfoqijwr82104182jdsaf;oj31u4.jpeg',
+  `photo_location` longtext DEFAULT NULL,
   `group_password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,8 +41,12 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`groupid`, `title`, `description`, `created_by`, `photo_location`, `group_password`) VALUES
-(1, 'Activities fair', 'At the RPI activities fair, students will get to visit all the clubs provided by RPI and have the opportunity to join them.', 1, 'uploads/48724i2bsakfnsaoidjfoqijwr82104182jdsaf;oj31u4.jpeg', 'nasd;fnjnslfjnlfnjsdljf'),
-(2, 'Monkey Club', 'Some primate species are recognized for their tree-swinging leaps that moved being acrobats to shame! Some monkey species take this “ arm at arm ” technique you may have seen children practicing on that “ monkey bars ” in the yard! Colobus monkeys, unlike other primate species, have hind legs that are much further than their forelimbs, creating for unbelievable leaping power with good velocity. In other words, monkeys are cool so come to our club interest meeting', 2, '/src/img/monkey.jpg', 'nlkfnslkfnlsf;snfkdnfjnsdf');
+(1, 'Activities fair', 'At the RPI activities fair, students will get to visit all the clubs provided by RPI and have the opportunity to join them.', 1, '/src/img/activities_fair.jpg', 'nasd;fnjnslfjnlfnjsdljf'),
+(2, 'BSA Fashion Show', 'The annual fashion show put on by the Black Students Alliance dazzled audiences with creative looks and models who walked with attitude and elegance. This walk, titled “Ice Me Out,” expressed two sides of luxe: opulent, glittery fabrics mixed with edgy streetwear looks and silhouettes. Cami tank dresses of velvet and sequined fabric were accessorized with oversized fur jackets and cross necklaces, and the menswear looks included brilliantly embroidered shirts in both modern and traditional African styles.', 2, '/src/img/monkey.jpg', 'nlkfnslkfnlsf;snfkdnfjnsdf'),
+(13, 'Mayors Cup Alumni and Fan Reception', 'Mark your calendar and plan to join other RPI hockey fans for a reception at the MVP Arena in Albany (formerly the Albany Times Union Center) prior to the RPI vs. Union Mayor\'s Cup games. Complimentary snacks and a cash bar will be available. Watch the alumni website and your email for registration information for the reception.\r\n\r\nAttendees of this event must agree to comply with all health and safety protocols required by the event sponsors, the venue, and New York State.', 1, NULL, 'CNJkz0G6rXQ3OgVr7tjZ'),
+(20, 'Denver | Social Gathering at The Novel Strand Brewing Company', 'Every year in Albany begins with a celebration of the region\'s evolving culinary scene. As flowers begin to bloom, Albany honors the city\'s treasured tulips & Dutch roots. During the summer, live music and festivals fill the weekends. As the leaves begin to change, Albany gets in on the hocus pocus with spooky stories and nightmarish thrills. The end of the year is capped off with merriment and holiday celebrations. Explore a small selection of Albany\'s annual events and festivals! ', 1, NULL, ''),
+(21, 'PearlPalooza', 'Downtown Albany\'s biggest all-day FREE music festival back! The all-ages festival,  featuring a diverse lineup of locally-and internationally-acclaimed performers, returns to North Pearl Street for its 12th year. Jam to live music, find your zen at Yogapalooza and grab a bite to eat from local vendors in the heart of downtown Albany!  The event is presented by SEFCU, in association with WEQX, Downtown Albany BID and Sugar Productions.', 1, NULL, 'W7NwgKS0RyhhrWF0Wxkg'),
+(22, 'Albany Tulip Fesitval', 'The City of Albany\'s Virtual Tulip Festival will be held throughout the month of May. Tune in virtually for performances by local musicians in unique locations, tours of the tulip beds with the City of Albany gardeners, kid-friendly videos and more! Watch on Albany Events YouTube, Albany Events Facebook or Public Access Channel Albany 1302. The 2021 Albany Tulip Queen and Court and Capital Region Mother of the Year will be presented at a later date with their own celebrations.', 1, NULL, '5Al0bLqFMGtb6hGWQJqm');
 
 -- --------------------------------------------------------
 
@@ -62,7 +66,10 @@ CREATE TABLE `pairing` (
 INSERT INTO `pairing` (`groupid`, `userid`) VALUES
 (1, 1),
 (2, 1),
-(2, 2);
+(2, 2),
+(13, 1),
+(21, 1),
+(22, 1);
 
 -- --------------------------------------------------------
 
@@ -79,7 +86,7 @@ CREATE TABLE `users` (
   `email` varchar(320) NOT NULL,
   `links` longtext DEFAULT NULL,
   `is_admin` tinyint(1) DEFAULT 0,
-  `photo_location` longtext DEFAULT 'uploads/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'
+  `photo_location` longtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -87,7 +94,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userid`, `username`, `pass`, `fname`, `lname`, `email`, `links`, `is_admin`, `photo_location`) VALUES
-(1, 'coolguy', '$2y$10$mkn/jjp8Vgpcua3EfkHT1e/7202YGbjDmTHiPNJixem9dn0h.SG8G', 'Connor', 'Silloway', 'silloc@rpi.edu', '{\"Instagram\":\"@connorsilloway\",\"LinkedIn\":\"www.linkedin.com\\/in\\/connor-silloway\"}', 1, 'uploads/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'),
+(1, 'coolguy', '$2y$10$mkn/jjp8Vgpcua3EfkHT1e/7202YGbjDmTHiPNJixem9dn0h.SG8G', 'Connor', 'Silloway', 'silloc@rpi.edu', '{\"Instagram\":\"@connorsilloway\",\"LinkedIn\":\"www.linkedin.com\\/in\\/connor-silloway\"}', 1, NULL),
 (2, 'bobbert', '$2y$10$jkw/3yQW7hXpidVM1uqmkuaKloFeu00oW6Xc96wWGje9cuUZv6MAq', 'bad guy', 'dumby', 'bad@gmail.com', NULL, 1, NULL),
 (4, 'g', '$2y$10$XiUJJBJgEb51ZzRKLjZAXuPsjra.tpb1qP.qeExXhiRu9ELFjLdpK', 'g', 'g', 'g', NULL, 0, NULL),
 (5, 'smitha4', '$2y$10$4pTBzgnMgFUMt29m6V04KOLaeXq2Em3JZoaDZ9utQQZOnknX43K5i', 'John', 'Smith', 'smitha4@rpi.edu', NULL, 0, NULL);
@@ -124,7 +131,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `groupid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `groupid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
