22ee1821c684cf3c99df7394002c17f4db204f54
alanabarth <bartha4@rpi.edu>
2021-12-09 12:22:06 -0500

    Update update-group.php
    had to fix error when not given a file to upload

--- update-group.php
+++ update-group.php
@@ -71,7 +71,7 @@
       // ASSUMING THAT $image_location will be given by you :)
       $image_location = $newfilename;
 
-      if ($_FILES['img']['size'] == 0 && $_FILES['img']['error'] == 0) { // check if image file exists
+      if ($_FILES['img']['size'] == 0 && $_FILES['img']['error'] == 4) { // check if image file exists
         // no image file
         // Don't update photo_location
         $stmt= "UPDATE groups
@@ -115,7 +115,7 @@
 
       $randomPass = generateRandomString(20); // random password, maybe hash it?
       $userID = $_SESSION['id'];
-      if ($_FILES['img']['size'] == 0 && $_FILES['img']['error'] == 0) { // check if image file exists
+      if ($_FILES['img']['size'] == 0 && $_FILES['img']['error'] == 4) { // check if image file exists
         // no image file
         // don't update photo_location
         $stmt = "INSERT INTO groups(title, `description`, created_by, group_password) 
@@ -125,6 +125,7 @@
       } else {
         // image file posted
         // update photo location
+        echo $_FILES['img']['size'] . ' error '.  $_FILES['img']['error'];
         include 'uploader_group.php';
         $stmt = "INSERT INTO groups(title, `description`, photo_location, created_by, group_password) 
                  VALUES(:title, :descr, :pholoc, :userID, :grpPass)";
